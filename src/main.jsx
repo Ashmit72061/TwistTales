6a00e3deab14f7fcc16b9027fcf70b1c0eb8b065
Ashmit Bindal <Ashmit72061@gmail.com>
2025-07-16 16:35:19 +0530

    small change (ignore)

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router-dom"; 
 import './styles/index.css'
-// import App from './App.jsx'
 import Router from './components/Router.jsx'
 import { AuthProvider } from './components/AuthContext.jsx'
 import { DataProvider } from './components/DataContext.jsx';
-
-import CustomCursor from './components/customCursor.jsx';
 
 createRoot(document.getElementById('root')).render(
     // <div className='cursor-none'>
